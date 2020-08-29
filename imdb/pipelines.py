5672c1b1e69da6c92db088c6fb367ee448aa3d13
Yash Mishra <yashmishra12@hotmail.com>
2020-08-30 05:09:25 +0530

    mongo DB

--- imdb/pipelines.py
+++ imdb/pipelines.py
@@ -6,8 +6,22 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import logging
+import pymongo
+
+class MongoDBPipeline:
+    collection_name = "best_movies"
+    @classmethod
+
+    def open_spider(cls, self, spider):
+        self.client = pymongo.MongoClient("mongodb+srv://yashmishra12:jgjjkkjhkj@cluster0.z847z.mongodb.net/<dbname>?retryWrites=true&w=majority")
+        self.db = self.client["IMDB"] #IMDB --> database name
 
 
-class ImdbPipeline:
+    def close_spider(self, spider):
+        self.client.close()
+
+
     def process_item(self, item, spider):
+        self.db[self.collection_name].insert(item)
         return item
--- imdb/settings.py
+++ imdb/settings.py
@@ -63,9 +63,9 @@ FEED_EXPORT_ENCODING = 'utf-8'
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'imdb.pipelines.ImdbPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'imdb.pipelines.MongoDBPipeline': 300, 
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
